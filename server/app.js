8998ba76d3128af6391df5517e7a8825df93914e
Fuzail22 <102959853+Fuzail22@users.noreply.github.com>
2023-09-18 13:10:18 +0530

    postioned cors properly

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(router);
-app.use(cors());
 
 app.listen(PORT, () => console.log("Server is listing @" + PORT));
